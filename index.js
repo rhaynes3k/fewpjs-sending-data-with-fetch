45bfec6c3f3084470f3af66f4dbff167972183fb
rhaynes3k <rhaynes3k@gmail.com>
2021-06-12 19:28:55 -0500

    commit tree 782399d4a750dbe7c1baa7a5f42dadd3f795f6f0

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+
+
+function submitData(name, email){
+  return fetch('http://localhost:3000/users',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        email
+      })
+    })
+    .then(function(resp){
+      return resp.json()
+    })
+    .then(function(data){
+      document.body.innerHTML = data.id
+    })
+    .catch(function(error){
+      document.body.innerHTML = error.message
+    })
+}
+
+// fetch(destinationURL, configurationObject)
+//   .then(function(resp){
+//     debugger
+//     return resp.json()
+//   })
+//   .then(function(object){
+//     console.log(object)
+//   })
+//   .catch(function(error){
+//     aler('Bad things! Ragnarok!')
+//     console.log(error.message)
+//   })
